0331bb4abda65bc5c30497c4c09a9756a34e3feb
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-01-23 15:16:51 -0500

    task 10: fix

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,7 +5,9 @@
 from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(
+    lst: Sequence[Any]
+) -> Union[Any, None]:
     """Returns the first element of the list as a float"""
     if lst:
         return lst[0]
