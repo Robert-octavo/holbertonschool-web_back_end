7a6090d425ec239e7c94d21e32992567c7e29e0d
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-01-23 14:31:20 -0500

    task 1. Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string """
+""" Returns the concatenation of str1 and str2 """
 
 
 def concat(str1: str, str2: str) -> str:
