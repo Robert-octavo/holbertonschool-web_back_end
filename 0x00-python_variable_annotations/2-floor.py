ca63760ed7d48b79b58f75b7ae27f9c6e72c9a5e
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-01-23 14:33:37 -0500

    task 2. Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-"""
-Write a type-annotated function floor which takes a float n as argument and returns the floor of the float.
-"""
+"""Returns the floor of the float"""
 
 import math
 
