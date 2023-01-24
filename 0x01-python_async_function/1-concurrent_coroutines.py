315ba7539db60dbb241bb44ce41da09a89be5dd0
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-01-24 12:18:30 -0500

    task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-"""Let's execute multiple coroutines at the same time with async"""
+"""Let's execute multiple coroutines at the same
+time with async
+"""
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
