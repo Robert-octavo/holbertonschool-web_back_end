98d28169b0c04f00140236724391a302466be561
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-01-24 12:24:04 -0500

    test 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,11 +4,12 @@
 time with async
 """
 import asyncio
+from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """[summary]
 
     Args:
@@ -18,7 +19,4 @@ async def task_wait_n(n: int, max_delay: int) -> list:
     Returns:
         list: [description]
     """
-    delays = []
-    for _ in range(n):
-        delays.append(wait_random(max_delay))
-    return [await delay for delay in asyncio.as_completed(delays)]
+    return await asyncio.create_task(wait_n(n, max_delay))
