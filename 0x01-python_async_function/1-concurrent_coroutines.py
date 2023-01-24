bfc0e1dbb73152034be1f2b6fa3bebfa18d6aaf2
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-01-24 11:03:38 -0500

    task 1. Let's execute multiple coroutines at the same time with async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Let's execute multiple coroutines at the same time with async"""
+import asyncio
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """[summary]
+
+    Args:
+        n (int): [description]
+        max_delay (int): [description]
+
+    Returns:
+        list: [description]
+    """
+    delays = []
+    for _ in range(n):
+        delays.append(wait_random(max_delay))
+    return [await delay for delay in asyncio.as_completed(delays)]
