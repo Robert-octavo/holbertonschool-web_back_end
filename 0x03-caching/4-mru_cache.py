ddd194bdab55cc496903f5c11194db57e4bada80
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-01-27 10:07:58 -0500

    test

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -2,6 +2,7 @@
 """Create a class MRUCache that inherits from BaseCaching
 and is a caching system:"""
 
+from collections import deque
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -12,17 +13,17 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """Constructor"""
         super().__init__()
-        self.keys = []
+        self.keys = deque()
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key and item:
-            if key in self.keys:
+            if key in self.cache_data:
                 self.keys.remove(key)
-            self.keys.append(key)
+            self.keys.appendleft(key)
             self.cache_data[key] = item
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                last = self.keys.pop(0)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last = self.keys.popleft()
                 del self.cache_data[last]
                 print("DISCARD: {}".format(last))
 
@@ -30,6 +31,6 @@ class MRUCache(BaseCaching):
         """Get an item by key"""
         if key in self.cache_data:
             self.keys.remove(key)
-            self.keys.append(key)
+            self.keys.appendleft(key)
             return self.cache_data[key]
         return None
