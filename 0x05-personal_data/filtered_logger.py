a593a7c952c2ea09da91efb3a6192059aee32a1f
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-02 21:59:32 -0500

    test

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,17 +6,38 @@ Write a function called filter_datum that returns the log message obfuscated:
         - fields: a list of strings representing all fields to obfuscate
         - redaction: a string representing by what the field will be obfuscated
         - message: a string representing the log line
-        - separator: a string representing by which character is separating all fields in the log line (message)
+        - separator: a string representing by which character is separating all
+        fields in the log line (message)
     The function should use a regex to replace occurrences of certain field values.
-    filter_datum should be less than 5 lines long and use re.sub to perform the substitution with a single regex
+    filter_datum should be less than 5 lines long and use re.sub to perform the
+    substitution with a single regex
 """
-
+import logging
 import re
 
+from typing import List
 
-def filter_datum(fields, redaction, message, separator):
+
+def filter_datum(fields: List[str], redaction, message, separator):
     """ returns the log message obfuscated """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
