dba0b44b29b02bb45a9649da056d39fd09714082
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-02 22:37:47 -0500

    test

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,15 +1,16 @@
-#!/urs/bin/env python3
-""" Write a function that takes in a password
-string and returns a salted,"""
+#!/usr/bin/env python3
+""" encrypt_password.py """
+
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ returns hashed password, which is a byte string """
+    """ return a password hashed with bcrypt """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ returns True if the password is valid for the
-    hashed password, otherwise it returns False """
+    """
+    Check if a password is valid
+    """
     return bcrypt.checkpw(password.encode(), hashed_password)
