d0f34c72e1094805b131e9f52e6b2cc494de7634
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-02 22:35:38 -0500

    test

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -7,3 +7,9 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ returns hashed password, which is a byte string """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ returns True if the password is valid for the
+    hashed password, otherwise it returns False """
+    return bcrypt.checkpw(password.encode(), hashed_password)
