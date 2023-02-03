8ac9595eb044d489f8cba8c616ed731275eb78ef
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-02 22:32:10 -0500

    task 5. Encrypting passwords

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,9 +1,9 @@
 #!/urs/bin/env python3
-""" Write a function that takes in a password string and returns
-a salted,"""
+""" Write a function that takes in a password
+string and returns a salted,"""
 import bcrypt
 
 
-def hash_password(password: str) -> str:
-    """ returns a salted, hashed password, which is a byte string """
+def hash_password(password: str) -> bytes:
+    """ returns hashed password, which is a byte string """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
