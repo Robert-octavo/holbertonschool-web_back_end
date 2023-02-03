9f6e64a3ebba9b2abb34e12f8ceb35b673417888
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-02 22:43:03 -0500

    correct pep

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -14,7 +14,8 @@ from typing import List
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """ returns the log message obfuscated """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
