3ee6b89277f8a9d4452c5086688b16a1fdd42cf0
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-07 23:49:38 -0500

    task 8. Basic - Base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Class BasicAuth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,18 @@ class BasicAuth(Auth):
         if authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """decode_base64_authorization_header method"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            encode = base64_authorization_header.encode('utf-8')
+            decode = base64.b64decode(encode)
+            return decode.decode('utf-8')
+        except:
+            return None
