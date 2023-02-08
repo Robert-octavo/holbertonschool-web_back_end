f309d7bf0d408363253bde05b0b6d2df159790c3
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-07 23:09:17 -0500

    test

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, abort, request
-from os import getenv
+#!/usr/bin/env python3
+"""
+Class Auth
+"""
+from flask import request
 from typing import List, TypeVar
-
-"""Class name Auth"""
 
 
 class Auth:
@@ -23,17 +24,8 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """authorization_header method"""
-        if request is None or 'Authorization' not in request.headers:
-            return None
-        return request.headers['Authorization']
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current_user method"""
         return None
-
-    def session_cookie(self, request=None):
-        """session_cookie method"""
-        if request is None:
-            return None
-        session_name = getenv('SESSION_NAME')
-        return request.cookies.get(session_name)
