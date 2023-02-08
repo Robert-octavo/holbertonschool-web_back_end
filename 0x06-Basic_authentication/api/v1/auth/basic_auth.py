0bf6547331dd36407e49915981d5a8938b6522a0
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-07 23:41:37 -0500

    test

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,7 +11,9 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """extract_base64_authorization_header method"""
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
             return None
         if authorization_header[:6] != 'Basic ':
             return None
