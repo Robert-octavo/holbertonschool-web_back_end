6b22ef5d85d5b8a383471ad15e21b04811cb8673
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-14 22:21:34 -0500

    task 5. Register user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -2,9 +2,29 @@
 """Auth module"""
 
 from db import DB
+from user import User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
 def _hash_password(password: str) -> str:
     """Hashes a password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> None:
+        """Register a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            self._db.add_user(email, _hash_password(password))
