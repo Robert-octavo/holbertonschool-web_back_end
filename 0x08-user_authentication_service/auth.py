b6c617aaeec6f8a27fa35151e9a12ab295fc5097
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-15 21:51:29 -0500

    test

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -10,7 +10,7 @@ import bcrypt
 
 def _hash_password(password: str) -> str:
     """Hashes a password"""
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 class Auth:
