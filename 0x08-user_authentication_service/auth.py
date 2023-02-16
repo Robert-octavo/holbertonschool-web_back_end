459fa28714f0c3be682dfe8ee983333d50deef9d
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-15 21:48:49 -0500

    return user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -20,14 +20,14 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> None:
+    def register_user(self, email: str, password: str) -> User:
         """Register a user"""
         try:
             user = self._db.find_user_by(email=email)
             if user:
                 raise ValueError("User {} already exists".format(email))
         except NoResultFound:
-            self._db.add_user(email, _hash_password(password))
+            user = self._db.add_user(email, _hash_password(password))
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validate login"""
--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -44,7 +44,6 @@ class DB:
         """
         if not kwargs:
             raise InvalidRequestError
-
         column = User.__table__.columns.keys()
 
         for key in kwargs.keys():
