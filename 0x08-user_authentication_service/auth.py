6953972cb6c041ca395f9e7c9c0952ab1b2816de
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-15 22:54:33 -0500

    task 13. Destroy session

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -67,3 +67,13 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a session"""
+        if user_id is None:
+            return None
+        try:
+            self._db.update_user(user_id, session_id=None)
+            return None
+        except NoResultFound:
+            return None
