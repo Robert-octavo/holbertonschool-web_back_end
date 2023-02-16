63115610e38a1ac5ce450fa88d3d0ecc920f3b92
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-15 23:10:28 -0500

    test

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -84,6 +84,6 @@ class Auth:
             user = self._db.find_user_by(email=email)
             token = _generate_uuid()
             self._db.update_user(user.id, reset_token=token)
-            return token
-        except NoResultFound:
+            return user.token
+        except Exception as e:
             raise ValueError
