5d93453489e40e4d1411243d74e17e9077e502e0
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-15 23:23:19 -0500

    test

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -34,8 +34,7 @@ class Auth:
                 raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             user = self._db.add_user(email, _hash_password(password))
-
-        return user
+            return user
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validate login"""
