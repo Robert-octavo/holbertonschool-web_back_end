997ddb94338af8b121dba2ed1dba024c78ba5a66
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-16 21:25:59 -0500

    Main

--- 0x08-user_authentication_service/main.py
+++ 0x08-user_authentication_service/main.py
@@ -44,6 +44,45 @@ def profile_unlogged() -> None:
     assert r.status_code == 403
 
 
+def profile_logged(session_id: str) -> None:
+    """Get profile when logged in"""
+    url = "http://localhost:5000/profile"
+    cookies = {"session_id": session_id}
+    r = requests.get(url, cookies=cookies)
+    assert r.status_code == 200
+    assert r.json() == {"email": EMAIL}
+
+
+def log_out(session_id: str) -> None:
+    """Log out"""
+    url = "http://localhost:5000/sessions"
+    cookies = {"session_id": session_id}
+    r = requests.delete(url, cookies=cookies)
+    assert r.status_code == 200
+    assert r.json() == {"message": "Bienvenue"}
+
+
+def reset_password_token(email: str) -> str:
+    """Get reset password token"""
+    url = "http://localhost:5000/reset_password"
+    data = {"email": email}
+    r = requests.post(url, data=data)
+    assert r.status_code == 200
+    reset_token = r.json().get("reset_token")
+    assert reset_token
+    return reset_token
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """Update password"""
+    url = "http://localhost:5000/reset_password"
+    data = {"email": email, "reset_token": reset_token,
+            "new_password": new_password}
+    r = requests.put(url, data=data)
+    assert r.status_code == 200
+    assert r.json() == {"email": email, "message": "Password updated"}
+
+
 if __name__ == "__main__":
 
     register_user(EMAIL, PASSWD)
