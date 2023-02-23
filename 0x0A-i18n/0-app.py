9c13a3d0e5ac95e3a6fad1700ba7924437c18b73
Robert Ortega <robert.ortega.octavo@gmail.com>
2023-02-23 17:37:16 -0500

    task 0

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basic app Flask app"""
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """Return index page"""
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
