8d8d5f0af926e9a070c047dff18ac569b0982b07
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-23 21:42:33 -0500

    test task 4

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Basic app Flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object('3-app.Config')
+
+
+def get_locale():
+    """Get locale"""
+    local = request.args.get('locale')
+    if local in app.config['LANGUAGES'] and local is not None:
+        return local
+    lang = request.accept_languages.best_match(app.config['LANGUAGES'])
+    return lang
+
+
+@app.route('/')
+def index():
+    """Return index page"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
