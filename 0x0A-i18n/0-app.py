ab35977dcc6d25b0e7495db2d1ba673ead6bf1b3
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-23 20:42:59 -0500

    test

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+
+@app.route('/')
 def index():
     """Return index page"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
