2af0ecddf19cce9fcb348b8a4806a0af266f730a
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-23 21:59:43 -0500

    text

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -4,8 +4,18 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
+def get_locale():
+    """Get locale"""
+    local = request.args.get('locale')
+    if local in app.config['LANGUAGES'] and local is not None:
+        return local
+    lang = request.accept_languages.best_match(app.config['LANGUAGES'])
+    return lang
+
+
 app = Flask(__name__)
-babel = Babel(app)
+babel = Babel(app, locale_selector=get_locale)
 
 
 class Config(object):
@@ -15,16 +25,7 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object('3-app.Config')
-
-
-def get_locale():
-    """Get locale"""
-    local = request.args.get('locale')
-    if local in app.config['LANGUAGES'] and local is not None:
-        return local
-    lang = request.accept_languages.best_match(app.config['LANGUAGES'])
-    return lang
+app.config.from_object('4-app.Config')
 
 
 @app.route('/')
