0183ec12828b8378a8a5e8f8a0949578dff9b345
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-23 20:39:11 -0500

    test

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -2,13 +2,25 @@
 """Basic app Flask app"""
 
 from flask import Flask, render_template, request
+from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Return index page"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
