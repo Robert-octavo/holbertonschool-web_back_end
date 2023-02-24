156c264b20e0b992e9b4509c2760f5758c29ada7
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-02-23 20:49:13 -0500

    add index file

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -21,7 +21,8 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """Get locale"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    lang = request.accept_languages.best_match(app.config['LANGUAGES'])
+    return lang
 
 
 @app.route('/')
