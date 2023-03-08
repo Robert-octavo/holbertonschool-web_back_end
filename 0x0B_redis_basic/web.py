9385d630f634a4e7e903c2ad76c3e0ee033c5ba6
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-07 21:26:04 -0500

    test

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,22 @@
+#!/urs/bin/env python3
+"""Web module"""
+
+import requests
+import redis
+
+
+def get_page(url: str) -> str:
+    """track how many times a particular URL was accessed"""
+    r = redis.Redis()
+    key = "count:" + url
+    if r.get(key):
+        r.incr(key)
+    else:
+        r.set(key, 1)
+    html = r.get(url)
+    if html:
+        return html.decode('utf-8')
+    else:
+        html = requests.get(url).text
+        r.setex(url, 10, html)
+        return html
