aa449c1c6a56b8b6cc930b1861939ca6e1551dac
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-09 22:14:38 -0500

    test 4

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -5,9 +5,6 @@
 --a new order.Quantity in the table items can be negative.
 
 
-CREATE TRIGGER decrease AFTER INSERT ON orders FOR 
-EACH ROW BEGIN 
-	UPDATE items
-	SET
-	    quantity = quantity - NEW.number
-	WHERE name = NEW.item_name;
+CREATE TRIGGER decrease
+AFTER INSERT ON orders FOR EACH ROW BEGIN UPDATE items SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
