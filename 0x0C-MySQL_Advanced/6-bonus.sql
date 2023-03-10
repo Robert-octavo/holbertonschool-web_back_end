a1ca884c81aa7feb67c2aba28281be9e66b1c147
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-09 22:51:22 -0500

    task

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,31 +1,39 @@
--- SQL script that creates a stored procedure
+-- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
 
-DELIMITER $$
+-- Procedure AddBonus is taking 3 inputs (in this order):
 
-CREATE PROCEDURE ADDBONUS(USER_ID INT, PROJECT_NAME 
-VARCHAR(255), SCORE INT) BEGIN 
-	SET @project_id = (
-	        SELECT id
-	        FROM projects
-	        WHERE
-	            name = project_name
-	    );
-	IF @project_id IS NULL THEN
-	INSERT INTO projects(name)
+-- user_id, a users.id value (you can assume user_id is linked to an existing users)
+
+-- project_name, a new or already exists projects - if no projects.name found in the
+
+-- table you should create it
+
+-- score, the score value for the correction
+
+CREATE PROCEDURE AddBonus(IN USER_ID INT, IN PROJECT_NAME 
+VARCHAR(255), IN SCORE INT) BEGIN 
+	DECLARE project_id INT;
+	DECLARE correction_id INT;
+	SELECT id INTO project_id
+	FROM projects
+	WHERE name = project_name;
+	IF project_id IS NULL THEN
+	INSERT INTO
+	    projects (name)
 	VALUES (project_name);
-	SET @project_id = (
-	        SELECT id
-	        FROM projects
-	        WHERE
-	            name = project_name
-	    );
+	SELECT
+	    id INTO project_id
+	FROM projects
+	WHERE name = project_name;
 	END IF;
 	INSERT INTO
-	    corrections(user_id, project_id, score)
-	VALUES (user_id, @project_id, score);
+	    corrections (user_id, project_id, score)
+	VALUES (user_id, project_id, score);
+	SELECT id INTO correction_id
+	FROM corrections
+	WHERE
+	    user_id = user_id
+	    AND project_id = project_id
+	    AND score = score;
+	INSERT INTO bonuses (correction_id) VALUES (correction_id);
 	END 
-$ 
-
-$ 
-
-DELIMITER;
