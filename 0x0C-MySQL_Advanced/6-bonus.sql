a6091c608f6b2a17b4d4627c12705adbe86eec3d
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-09 23:43:05 -0500

    test

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -14,6 +14,7 @@ CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score
 BEGIN SET @project = (SELECT id FROM projects WHERE name = project_name);
 	IF @project IS NULL THEN
 		INSERT INTO projects (name) VALUES (project_name);
+		SET @project = (SELECT id FROM projects WHERE name = project_name);
 	END IF;
 INSERT INTO corrections(user_id, project_id, score) VALUES (user_id, @project, score);
 END; //
