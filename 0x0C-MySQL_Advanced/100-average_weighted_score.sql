d51e5a4f339bc0e3c10dffa92ef7d4985d842084
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-11 19:11:14 -0500

    test

---
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,25 @@
+-- creates a store procedure ComputeAverageWeightedScoreForUser
+
+-- that computes and store the average wieghted score for a student
+
+-- take one input
+
+-- user_id value ADD
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser
+(IN user_id INT) BEGIN 
+	DECLARE avg_score FLOAT;
+	SELECT
+	    AVG(score * weight) INTO avg_score
+	FROM AS u JOIN corrections as c ON u.id=c.user_id
+	JOIN projects AS p ON c.project_id=p.id
+	WHERE id = user_id;
+	UPDATE users
+	SET
+	    average_score = avg_score
+	WHERE id = user_id;
+END$$ 
+
+DELIMITER;
