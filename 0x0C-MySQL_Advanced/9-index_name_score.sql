8c8d18603bb0ea92c459c79043c4ffe3bedb1ad9
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-11 19:00:03 -0500

    task

--- 0x0C-MySQL_Advanced/9-index_name_score.sql
+++ 0x0C-MySQL_Advanced/9-index_name_score.sql
@@ -1,7 +1,3 @@
--- Create an index idx_name_first_score on the table names
-
--- and the first letter of name and the score
-
---Only the fisrt letter of the name and score must be indexed
+-- Optimize search and score
 
 CREATE INDEX idx_name_first_score ON names (name(1), score);
