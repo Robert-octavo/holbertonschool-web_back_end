74a523ab9109055187dca1b0414a4103a1a6e35f
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-14 21:46:27 -0500

    test

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Python function that returns all students sorted by average score:"""
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score"""
+    students = mongo_collection.aggregate([
+        {"$group": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return students
