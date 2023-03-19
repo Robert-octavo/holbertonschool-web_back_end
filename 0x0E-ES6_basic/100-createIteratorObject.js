76f2caf150c4edd277f33f51ddd33239d7c3fd46
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-18 19:00:00 -0500

    fix

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,17 +1,17 @@
 export default function createIteratorObject(report) {
-    let index = 0;
-    const employees = [];
-    for (const department of Object.values(report.allEmployees)) {
-        for (const employee of department) {
-            employees.push(employee);
-        }
+  let index = 0;
+  const employees = [];
+  for (const department of Object.values(report.allEmployees)) {
+    for (const employee of department) {
+      employees.push(employee);
     }
-    return {
-        next() {
-            let value = employees[index];
-            let done = index >= employees.length;
-            index++;
-            return { value, done };
-        }
-    };
+  }
+  return {
+    next() {
+      const value = employees[index];
+      const done = index >= employees.length;
+      index += 1;
+      return { value, done };
+    },
+  };
 }
