6189be5ec7d77495c1ab550456e9c86f8a863187
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-21 22:06:29 -0500

    test

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -22,5 +22,14 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ])
+    .then((results) => {
+      for (const result of results) {
+        if (result.status === 'rejected') {
+          result.value = `Error: ${result.reason.message}`;
+          delete result.reason;
+        }
+      }
+      return results;
+    });
 }
