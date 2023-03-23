8a3d8ecb54b357f3b60c5cae92ce6a7a43390baa
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-03-22 22:06:27 -0500

    task 0

---
+++ 0x10-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/*
+mplement a class named ClassRoom:
+
+  - Prototype: export default class ClassRoom
+  - It should accept one attribute named maxStudentsSize (Number)
+    and assigned to _maxStudentsSize
+*/
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
