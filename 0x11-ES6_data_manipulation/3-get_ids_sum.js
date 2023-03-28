09e0c53555521ee5e03a7898918faa2dd0e7efa4
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2023-03-28 12:33:37 -0500

    Task 3

---
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+/*
+Create a function getStudentIdsSum that returns the sum of all the student ids.
+It should accept a list of students (from getListStudents) as a parameter.
+You must use the reduce function on the array.
+*/
+export default function getStudentIdsSum(getListStudents) {
+  return getListStudents.reduce((sum, student) => sum + student.id, 0);
+}
