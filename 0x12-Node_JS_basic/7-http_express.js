aff83bc361d2a8b6f2864d4d3a35fddf17e23f43
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2023-04-03 15:58:10 -0500

    test task 7

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,35 @@
+/*
+recreate the small HTTP server using Express:
+
+- It should be assigned to the variable app and this one must be exported
+- HTTP server should listen on port 1245
+- It should return plain text
+- When the URL path is /, it should display Hello Holberton School! in the page body
+- When the URL path is /students, it should display This is the list of our students
+followed by the same content as the file 3-read_file_async.js (with and without the database)
+ - the name of the database must be passed as argument of the file
+CSV file can contain empty lines (at the end) - and they are not a valid student!
+*/
+
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.write('This is the list of our students\n');
+  try {
+    const students = await countStudents(process.argv[2]);
+    res.end(students.join('\n'));
+  } catch (error) {
+    res.end(error.message);
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
