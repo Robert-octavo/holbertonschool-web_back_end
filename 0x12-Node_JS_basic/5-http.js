8f37825844f0688b6fcb1cd697de7a3ca992fa1a
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2023-04-03 15:22:01 -0500

    test

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,63 @@
+/*
+create a small HTTP server using the http module:
+
+- It should be assigned to the variable app and this one must be exported
+- HTTP server should listen on port 1245
+- It should return plain text
+- When the URL path is /, it should display Hello Holberton School! in the page body
+- When the URL path is /students, it should display This is the list of our students
+  followed by the same content as the file 3-read_file_async.js (with and without the database)
+    - the name of the database must be passed as argument of the file
+- CSV file can contain empty lines (at the end) - and they are not a valid student!
+*/
+
+/* const http = require('http'); // import the http module
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => { // create the server
+  res.statusCode = 200; // set the status code
+  res.setHeader('Content-Type', 'text/plain'); // set the header
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+        const students = await countStudents(process.argv[2]);
+        res.write(students.join('\n'));
+    } catch (error) {
+        res.write(error.message);
+    }
+    res.statusCode = 400;
+    res.end();
+  }
+});
+
+app.listen(1245); // listen on port 1245
+
+module.exports = app; */
+
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const localhost = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(process.argv[2]);
+      res.write(students.join('\n'));
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+}).listen(port, localhost);
+
+module.exports = app;
