43f671e283bc5a872facd574b1cbfd585e4a74f5
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-04-06 18:49:21 -0500

    test

--- 0x13-unittests_in_js/9-api/api.js
+++ 0x13-unittests_in_js/9-api/api.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
+app.get('/cart/:id[0-9]+', (req, res) => {
   res.send(`Payment methods for cart ${req.params.id}`);
 });
 
