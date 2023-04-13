43e689f620346029c7ee964f6d78258c0424e46f
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-04-12 21:16:30 -0500

    task 0

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,18 @@
+/*
+Using Babel and ES6, write a script named 0-redis_client.js. It should connect to the Redis server running on your machine:
+
+- It should log to the console the message Redis client connected to the server when the connection to Redis works correctly
+- It should log to the console the message Redis client not connected to the server: ERROR_MESSAGE when the connection to Redis does not work
+*/
+
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
