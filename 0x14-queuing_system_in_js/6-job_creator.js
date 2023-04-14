84e8112ddfe2170df6475a6c724c07e7be5c3a7c
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-04-13 21:30:49 -0500

    task 6 and 7

---
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,42 @@
+/*
+
+  - Create a queue with Kue
+  - Create an object containing the Job data with the following format:
+
+{
+  phoneNumber: string,
+  message: string,
+}
+
+  - Create a queue named push_notification_code, and create a job with the object created before
+  - When the job is created without error, log to the console Notification job created: JOB ID
+  - When the job is completed, log to the console Notification job completed
+  - When the job is failing, log to the console Notification job failed
+*/
+
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account'
+};
+
+const job = queue.create('push_notification_code', jobData)
+  .save((error) => {
+    if (!error) console.log(`Notification job created: ${job.id}`);
+  }
+  );
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', (errorMessage) => {
+  console.log(`Notification job failed: ${errorMessage}`);
+});
+
+job.on('progress', (progress, data) => {
+  console.log(`Notification job ${job.id} ${progress}% complete`);
+});
